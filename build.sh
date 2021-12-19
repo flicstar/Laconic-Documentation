83568af56ec71ca844de39d412cdccfba7c77432
Robert Douglass <robert@Roberts-MBP.localdomain>
2021-12-20 00:43:47 +0100

    get hugo extended

--- build.sh
+++ build.sh
@@ -6,12 +6,17 @@ tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_0.91.0_Linux-64bit.tar.gz
-tar -xf hugo_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
+#get https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_0.91.0_Linux-64bit.tar.gz
+# tar -xf hugo_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
+#source /root/.bashrc
+
+mkdir src
+cd src
+git clone https://github.com/gohugoio/hugo.git
+cd hugo
+go install --tags extended
 source /root/.bashrc
 
-go version
-hugo version
-
+cd
 hugo
 
