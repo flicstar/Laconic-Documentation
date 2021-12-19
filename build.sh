914b89b9f5217690f9f0b69fa546baa610e4589f
Robert Douglass <robert@Roberts-MBP.localdomain>
2021-12-19 19:58:05 +0100

    try getting hugo and go myself

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
+
+HUGO_VERSION = 0.91.0
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz && \
+    tar -xf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -C /usr/local/bin && \
+    hugo version && rm hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+    
+
