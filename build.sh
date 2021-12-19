e56daf54d32a0b3221fde2091eeda8884a35a896
Robert Douglass <robert@Roberts-MacBook-Pro.local>
2021-12-19 23:17:46 +0100

    I really have to learn proper bash

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ sudo tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 HUGO_TAR = https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_0.91.0_Linux-64bit.tar.gz
-wget $HUGO_TAR
-tar -xf $HUGO_TAR -C /usr/local/bin
+wget ${HUGO_TAR}
+tar -xf ${HUGO_TAR} -C /usr/local/bin
 source /root/.bashrc
 
 go version
