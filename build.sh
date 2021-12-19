d2d3c7771b6b7373acbf0974f0516c2a67b87194
Robert Douglass <robert@Roberts-MacBook-Pro.local>
2021-12-19 23:15:30 +0100

    try again with different script

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 apt-get update && apt-get install -y wget
 
-wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
+wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
 
-HUGO_VERSION = 0.91.0
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-tar -xf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -C /usr/local/bin
-
+HUGO_TAR = https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_0.91.0_Linux-64bit.tar.gz
+wget $HUGO_TAR
+tar -xf $HUGO_TAR -C /usr/local/bin
 source /root/.bashrc
+
 go version
 hugo version
 
-rm https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh
-rm hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
     
 
