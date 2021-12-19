e8bddd368a8d547439bb6027eb4fcbfcb02b58e0
Robert Douglass <robert@Roberts-MacBook-Pro.local>
2021-12-19 23:19:13 +0100

    without sudo

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 apt-get update && apt-get install -y wget
 
 wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 HUGO_TAR = https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_0.91.0_Linux-64bit.tar.gz
