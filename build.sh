a0d5254e0b4d83ddba4ee15b0c8514693ac1af35
Robert Douglass <robert@Roberts-MBP.localdomain>
2021-12-20 02:14:47 +0100

    so much for -nv

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-apt-get update && apt-get install -y wget git
+apt-get update && apt-get install -y wget 
 
 wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
-tar -C /usr/local -xzf -nv go1.13.linux-amd64.tar.gz
+tar -C /usr/local -xzf  go1.13.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 wget https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_extended_0.91.0_Linux-64bit.tar.gz
-tar -xf -nv hugo_extended_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
+tar -xzf  hugo_extended_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
 hugo version && rm hugo_extended_0.91.0_Linux-64bit.tar.gz
 source /root/.bashrc
 
