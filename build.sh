569750ef4b8257be330c9e85a6c4f9de2880c824
Robert Douglass <robert@Roberts-MBP.localdomain>
2021-12-20 01:15:02 +0100

    Maybe ?

--- build.sh
+++ build.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 apt-get update && apt-get install -y wget git
-source /root/.bashrc
-
-git status
-
-exit
-
 
 wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
-
-
-#get https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_0.91.0_Linux-64bit.tar.gz
-# tar -xf hugo_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
-#source /root/.bashrc
+source /root/.bashrc
 
 mkdir src
 cd src
