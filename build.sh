05dd0783dcdffd322a2fd3d3d78274f30522e26a
Robert Douglass <rob@robshouse.net>
2022-01-27 10:48:50 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ export PATH=$PATH:/usr/local/go/bin
 wget https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_extended_0.91.0_Linux-64bit.tar.gz
 tar -xzf  hugo_extended_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
 hugo version && rm hugo_extended_0.91.0_Linux-64bit.tar.gz
-source /root/.bashrc
+ls -al /root
+#source /root/.bashrc
 
 hugo --minify
 
