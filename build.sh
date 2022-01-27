a118ae2964ef2965d4f17fc1f61b905dacfacaac
Robert Douglass <rob@robshouse.net>
2022-01-27 10:52:05 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 apt-get update && apt-get install -y wget 
 
-wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
-tar -C /usr/local -xzf  go1.13.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+#wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+#tar -C /usr/local -xzf  go1.13.linux-amd64.tar.gz
+#export PATH=$PATH:/usr/local/go/bin
 
 wget https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_extended_0.91.0_Linux-64bit.tar.gz
 tar -xzf  hugo_extended_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
-hugo version && rm hugo_extended_0.91.0_Linux-64bit.tar.gz
-ls -al /root
-#source /root/.bashrc
+export PATH=$PATH:/usr/local/bin/hugo
 
 hugo --minify
 
