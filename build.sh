2da555637af20b7b05c2c465d5b580b960c3f360
Robert Douglass <rob@robshouse.net>
2022-01-27 12:42:50 +0100

    Update to 0.92.0

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt-get update && apt-get install -y wget 
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_extended_0.91.0_Linux-64bit.tar.gz
-tar -xzf  hugo_extended_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
+wget https://github.com/gohugoio/hugo/releases/download/v0.92.0/hugo_extended_0.92.0_Linux-64bit.tar.gz
+tar -xzf  hugo_extended_0.92.0_Linux-64bit.tar.gz -C /usr/local/bin
 source ~/.bashrc
 
 hugo version
