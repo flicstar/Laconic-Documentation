2cdebd4e1b72e96be5d3bd01515291bf71f40ed4
Robert Douglass <rob@robshouse.net>
2022-01-27 11:20:08 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,12 +7,9 @@ apt-get update && apt-get install -y wget
 
 wget https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_extended_0.91.0_Linux-64bit.tar.gz
 tar -xzf  hugo_extended_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
-ls -al /usr/local/bin/hugo
-echo $PATH
-. /etc/profile
-. /usr/local/bin/hugo
-hugo version
-hugo --minify
+
+/usr/local/bin/hugo version
+/usr/local/bin/hugo --minify
 
 
 
