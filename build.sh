e28743abf7b82163496c9109e0aa9d5de17d8997
Robert Douglass <rob@robshouse.net>
2022-01-27 11:31:30 +0100

    Note: changing from alpine to ubuntu on Fleek solved all the problems.

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@ apt-get update && apt-get install -y wget
 
 wget https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_extended_0.91.0_Linux-64bit.tar.gz
 tar -xzf  hugo_extended_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
-echo "ls -al ~"
-ls -al ~
 source ~/.bashrc
 
 hugo version
